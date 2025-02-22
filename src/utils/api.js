7d34675a3859d876edb0f69f39e453ff72d24e37
sakshuu <sakshisjadhav.120@gmail.com>
2025-02-22 20:42:04 +0530

    updated with env - rapid api key

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,15 +1,32 @@
 import axios from "axios";
 
+// const BASE_URL = "https://youtube138.p.rapidapi.com";
 const BASE_URL = "https://youtube138.p.rapidapi.com";
 
+// const options = {
+//   params: { hl: "en", gl: "US" },
+//   headers: {
+//     "X-RapidAPI-Key": process.env.REACT_APP_YOUTUBE_API_KEY || "YOUR_API_KEY",
+//     "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
+//   },
+// };
+
 const options = {
-  params: { hl: "en", gl: "US" },
-  headers: {
-    "X-RapidAPI-Key": process.env.REACT_APP_YOUTUBE_API_KEY || "YOUR_API_KEY",
-    "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
+  // method: 'GET',
+  // url: 'https://youtube138.p.rapidapi.com/auto-complete/',
+  params: {
+    hl: 'en',
+    gl: 'US'
   },
+  headers: {
+    'x-rapidapi-key': process.env.REACT_APP_YOUTUBE_API_KEY || "YOUR_API_KEY",
+    'x-rapidapi-host': 'youtube138.p.rapidapi.com'
+  }
 };
 
+axios.get("YOUR_API_URL", options)
+  .then(response => console.log(response.data))
+  .catch(error => console.error(error));
 export const fetchDataFromApi = async (url) => {
   const { data } = await axios.get(`${BASE_URL}/${url}`, options);
   return data;
